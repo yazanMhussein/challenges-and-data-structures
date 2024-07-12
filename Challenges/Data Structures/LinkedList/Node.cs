a93473f40d30fd45bcc1f9163d7f601e3cc3522d
yazanMhussein <yazan.m.hussein@gmail.com>
2024-07-12 21:51:33 +0300

    didn't finsh

---
+++ Challenges/Data Structures/LinkedList/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using namespace LinkedList
+{
+    public class Node
+    {
+        public int Data;
+        public Node Next;
+
+        public Node(int data, Node next) {
+            Data = data;
+            Next = next;    
+        }
+    }
+}
