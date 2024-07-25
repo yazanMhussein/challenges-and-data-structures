3ce97613d966aa569b016261b19b2e1748126c63
yazanMhussein <yazan.m.hussein@gmail.com>
2024-07-25 11:49:38 +0300

    test

---
+++ Challenges/Data Structures/ki/Node.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Stack_Queue
+    
+public class Node
+{
+    public int Data { get; set; }
+    public Node Next { get; set; }
+
+    public Node(int data) 
+    { 
+        Data = data;
+        Next = null;  
+    }
+}
+
