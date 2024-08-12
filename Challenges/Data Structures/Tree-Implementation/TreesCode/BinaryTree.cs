db51a8c293d94b1168bdc469e78e43675ed73749
yazanMhussein <yazan.m.hussein@gmail.com>
2024-08-12 17:18:44 +0300

    test

---
+++ Challenges/Data Structures/Tree-Implementation/TreesCode/BinaryTree.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreesCode
+{
+    public class BinaryTree
+    {
+        public TNode Root { get; set; }
+
+
+        public BinaryTree(int rootValue)
+        {
+            Root = new TNode(rootValue);
+        }
+
+
+        public void Insert(int value)
+        {
+            Insert(Root, value);
+        }
+
+        private void Insert(TNode node, int value)
+        {
+            if (value < node.Value)
+            {
+                if (node.Left == null)
+                {
+                    node.Left = new TNode(value);
+                }
+                else
+                {
+                    Insert(node.Left, value);
+                }
+            }
+
+            else
+            {
+                if (node.Right == null)
+                {
+                    node.Right = new TNode(value);
+                }
+                else
+                {
+                    Insert(node.Right, value);
+                }
+            }
+        }
+
+        // ( Root - Left - Right )
+        public void PreOrderTraversal(TNode node)
+        {
+            if (node == null) return; // Base case if node null then return
+
+            Console.Write(node.Value + "  ");
+            PreOrderTraversal(node.Left);
+            PreOrderTraversal(node.Right);
+
+        }
+
+        // ( Left - Root - right )
+        public void InOrderTraversal(TNode node)
+        {
+            if (node == null) return;
+
+            InOrderTraversal(node.Left);
+            Console.Write(node.Value + "  ");
+            InOrderTraversal(node.Right);
+
+        }
+
+        // ( Left - Right - Root )
+        public void PostOrderTraversal(TNode node)
+        {
+            if (node == null) return; // Base case if node is null then return
+            PostOrderTraversal(node.Left);
+            PostOrderTraversal(node.Right);
+            Console.WriteLine(node.Value + " ");
+        }
+        public void Print(TNode node, string indent = "", bool last = true)
+        {
+            if (node != null)
+            {
+                Console.Write(indent);
+                if (last)
+                {
+                    Console.Write("└─");
+                    indent += "  ";
+                }
+                else
+                {
+                    Console.Write("├─");
+                    indent += "| ";
+                }
+                Console.WriteLine(node.Value);
+
+                Print(node.Left, indent, false);
+                Print(node.Right, indent, true);
+            }
+        }
+    }
+}
+ 
---
+++ Challenges/Data Structures/Tree-Implementation/TreesCode/TNode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreesCode
+{
+    public class TNode
+    {
+        public int Value { get; set; }
+
+        public TNode Left { get; set; }
+
+        public TNode Right { get; set; }
+
+        public TNode(int value)
+        {
+            Value = value;
+            Left = null;
+            Right = null;
+        }
+    }
+}
